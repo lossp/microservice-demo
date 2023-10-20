aae894a15e3f9a73a4802607f5b99bc7d09ea596
lossp <richard.xxmxx@gmail.com>
2023-10-20 16:10:15 +0800

    [Root] downgrade the spring boot version to 3.0.12. making it compatible for Nacos

--- transaction/transaction-server/src/main/java/com/lossp/microservice/transactionserver/TransactionServerApplication.java
+++ transaction/transaction-server/src/main/java/com/lossp/microservice/transactionserver/TransactionServerApplication.java
@@ -2,9 +2,11 @@ package com.lossp.microservice.transactionserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.lossp.microservice.transactionimpl")
+@EnableDiscoveryClient
 public class TransactionServerApplication {
 
     public static void main(String[] args) {
