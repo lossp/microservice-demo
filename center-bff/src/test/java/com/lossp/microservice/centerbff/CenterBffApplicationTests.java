ac2116c6ec5cc5c9584ec1841da424bf9cf21514
lossp <richard.xxmxx@gmail.com>
2023-10-20 11:01:39 +0800

    [center-bff] init the basic project and bff part

---
+++ center-bff/src/test/java/com/lossp/microservice/centerbff/CenterBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lossp.microservice.centerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
