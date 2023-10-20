ce2d7a32f32c0442dfd7a4a9481f21942126955f
lossp <richard.xxmxx@gmail.com>
2023-10-20 17:17:26 +0800

    [Transaction] add dubbo service to expose the save method

--- transaction/transaction-impl/src/main/java/com/lossp/microservice/transactionimpl/service/TransactionService.java
+++ transaction/transaction-impl/src/main/java/com/lossp/microservice/transactionimpl/service/TransactionService.java
@@ -1,10 +1,12 @@
 package com.lossp.microservice.transactionimpl.service;
 
 import com.lossp.microservice.transactionintf.transaction.ITransactionService;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service
 public class TransactionService implements ITransactionService {
     @Override
     public void saveTransaction() {
-        // doNothing
+        System.out.println("========the transaction service is saving the order !!!");
     }
 }
