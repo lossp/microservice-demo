73d243921794ad12bf2dfbf269fb3ef636333143
lossp <richard.xxmxx@gmail.com>
2023-10-26 12:13:05 +0800

    [Transaction] build up the transaction server package

---
+++ transaction/transaction-server/src/test/java/com/lossp/microservice/transactionserver/TransactionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lossp.microservice.transactionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
