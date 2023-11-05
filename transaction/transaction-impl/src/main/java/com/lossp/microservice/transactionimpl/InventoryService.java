16935b58e5974b933f205d8ed9ce7e0933505ba3
lossp <richard.xxmxx@gmail.com>
2023-11-05 15:29:10 +0800

    [Transaction] integrate the redisson with spring boot and add configuration to start redis as standalone model

--- transaction/transaction-impl/src/main/java/com/lossp/microservice/transactionimpl/InventoryService.java
+++ transaction/transaction-impl/src/main/java/com/lossp/microservice/transactionimpl/InventoryService.java
@@ -1,14 +1,24 @@
 package com.lossp.microservice.transactionimpl;
 
 
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
 public class InventoryService {
+    private final RedissonClient redissonClient;
+
+    public InventoryService(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
+
     @PostConstruct
     public void printTest() {
+        redissonClient.getBucket("Test Phase").set("The tt");
         System.out.println("---------");
+        String o =(String) redissonClient.getBucket("Test Phase").get();
+        System.out.println("-======0" + o);
     }
 }
